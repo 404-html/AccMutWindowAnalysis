e9023083b5453dc806ca96f2b2172bd1d8ad9266
lsrcz <lsrcz@pku.edu.cn>
2019-03-15 02:20:44 +0800

    tests dir existence
    Signed-off-by: Sirui Lu <lsrcz@pku.edu.cn>

--- tools/test-runtime/main.cpp
+++ tools/test-runtime/main.cpp
@@ -7,6 +7,8 @@
 int main() {
     // system("rm -rf /tmp/accmut_test");
     // mkdir("/tmp/accmut_test", 0755);
+    if (!access("/tmp/accmut_test", F_OK))
+        mkdir("tmp/accmut_test", 0755);
     chdir("/tmp/accmut_test");
     //unixrunner or1;
     // stdio_positioning_runner for1;
