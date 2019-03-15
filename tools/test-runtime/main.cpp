8b262e353f2e6593b74500321b563630c5e94a51
lsrcz <lsrcz@pku.edu.cn>
2019-03-15 12:12:48 +0800

    Run all test
    Signed-off-by: Sirui Lu <lsrcz@pku.edu.cn>

--- tools/test-runtime/main.cpp
+++ tools/test-runtime/main.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "test-stdio.h"
+#include "test-unix.h"
 
 int main() {
     // system("rm -rf /tmp/accmut_test");
@@ -10,13 +11,13 @@ int main() {
     if (!access("/tmp/accmut_test", F_OK))
         mkdir("tmp/accmut_test", 0755);
     chdir("/tmp/accmut_test");
-    //unixrunner or1;
-    // stdio_positioning_runner for1;
-    // stdio_file_status_runner fsr1;
-    // stdio_unix_interoperate_runner fuir1;
-    // stdio_input_runner fir1;
-    // stdio_scanf_runner ssr1;
-    // stdio_fread_fwrite_runner sffr1;
-    // stdio_printf_runner spr1;
+    unix_runner or1;
+    stdio_positioning_runner for1;
+    stdio_file_status_runner fsr1;
+    stdio_unix_interoperate_runner fuir1;
+    stdio_input_runner fir1;
+    stdio_scanf_runner ssr1;
+    stdio_fread_fwrite_runner sffr1;
+    stdio_printf_runner spr1;
     stdio_freopen_runner sfr1;
 }
